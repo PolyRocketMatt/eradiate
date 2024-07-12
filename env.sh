303047a4c194593f66d9139d51fdbd7ed867c9e3
PolyRocketMatt <rocketpencil.studios@gmail.com>
2024-07-12 15:48:19 +0200

    Add notebook to test Fresnel

---
+++ env.sh
@@ -0,0 +1,5 @@
+TMP_PATH="/home/matt/eradiate/ext/mitsuba/build:/home/matt/.opam/default/bin:/home/matt/.local/bin:/home/matt/anaconda3/envs/eradiate/bin:/home/matt/anaconda3/condabin:/home/matt/.modular/pkg/packages.modular.com_mojo/bin:/home/matt/.modular/pkg/packages.modular.com_max/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/lib/wsl/lib"
+
+
+export PATH="$TMP_PATH"
+
