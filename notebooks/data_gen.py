df60fbb4f53271fbca839724b4114f18120d4d52
Matthias <rocketpencil.studios@gmail.com>
2024-07-25 17:17:47 +0200

    Updates to better compare Mitsuba and 6S

--- notebooks/data_gen.py
+++ notebooks/data_gen.py
@@ -5,79 +5,96 @@ from multiprocessing.dummy import Pool as ThreadPool
 from Py6S import *
 from tqdm import tqdm
 
-wind_azimuth = 0
+wind_direction = 0
 salinity = 34.3
 pigmentation = 0.3
 
 # Boundary Parameters
 outgoing_azimuth = 180
-outgoing_zeniths = np.linspace(0, np.deg2rad(89), 18)    
+outgoing_zeniths = np.linspace(0, np.deg2rad(89), 45)    
 
 incoming_azimuth = 0
-incoming_zeniths = np.linspace(0, np.deg2rad(89), 18)
+incoming_zeniths = np.linspace(0, np.deg2rad(89), 45)
 
 # All dataset wind speeds
-#wind_speeds = [0.1, 1, 5, 10, 37]
-wind_speeds = [1, 10, 37]
+wind_speeds = [0.1, 1, 10, 37]
+wavelengths = [0.4, 0.5, 0.7, 1.5, 2.2]
 
-def generate_data(wind_speed, pbar):
-    # SixS Parameters
-    s = SixS()
-    s.wavelength = Wavelength(0.5)
-    s.atmos_profile = AtmosProfile.PredefinedType(AtmosProfile.NoGaseousAbsorption)
-    s.aero_profile = AeroProfile.PredefinedType(AeroProfile.NoAerosols)
-    s.ground_reflectance = GroundReflectance.HomogeneousOcean(wind_speed, wind_azimuth, salinity, pigmentation)
+def generate_data(wavelength, wind_speed, pbar):
+    try:
+        # SixS Parameters
+        s = SixS()
+        s.wavelength = Wavelength(wavelength)
+        s.atmos_profile = AtmosProfile.PredefinedType(AtmosProfile.NoGaseousAbsorption)
+        s.aero_profile = AeroProfile.PredefinedType(AeroProfile.NoAerosols)
+        s.ground_reflectance = GroundReflectance.HomogeneousOcean(wind_speed, wind_direction, salinity, pigmentation)
 
-    # Keep a list of temporary file names
-    solar_zenith_data = {}
+        # Keep a list of temporary file names
+        solar_zenith_data = {}
 
-    # Loop over tuple of outgoing azimuths and zeniths
-    for incoming_zenith in incoming_zeniths:
-        reflectances = {}
-        for outgoing_zenith in outgoing_zeniths:
-            s.geometry = Geometry.User()
-            s.geometry.solar_z = np.rad2deg(incoming_zenith)
-            s.geometry.solar_a = np.rad2deg(incoming_azimuth)
-            s.geometry.view_z = np.rad2deg(outgoing_zenith)
-            s.geometry.view_a = np.rad2deg(outgoing_azimuth)
-            s.run()
-            foam = s.outputs.values['water_component_foam']
-            glint = s.outputs.values['water_component_glint']
-            water = s.outputs.values['water_component_water']
-            total = foam + glint + water
-            foam_percent = foam / total
-            glint_percent = glint / total
-            water_percent = water / total
-            reflectances[outgoing_zenith] = {
-                'foam': foam,
-                'glint': glint,
-                'water': water,
-                'total': total,
-                'foam_percent': foam_percent,
-                'glint_percent': glint_percent,
-                'water_percent': water_percent
-            }
+        # Loop over tuple of outgoing azimuths and zeniths
+        for incoming_zenith in incoming_zeniths:
+            reflectances = {}
+            data_index = 0
+            for outgoing_zenith in outgoing_zeniths:
+                s.geometry = Geometry.User()
+                s.geometry.solar_z = np.rad2deg(incoming_zenith)
+                s.geometry.solar_a = np.rad2deg(incoming_azimuth)
+                s.geometry.view_z = np.rad2deg(outgoing_zenith)
+                s.geometry.view_a = np.rad2deg(outgoing_azimuth)
+                s.run()
+                foam = s.outputs.values['water_component_foam']
+                glint = s.outputs.values['water_component_glint']
+                water = s.outputs.values['water_component_water']
+                total = foam + glint + water
+                if (total == 0):
+                    foam_percent = 0
+                    glint_percent = 0
+                    water_percent = 0
+                else:
+                    foam_percent = foam / total
+                    glint_percent = glint / total
+                    water_percent = water / total
+                reflectances[data_index] = {
+                    'outgoing_zenith': outgoing_zenith,
+                    'foam': foam,
+                    'glint': glint,
+                    'water': water,
+                    'total': total,
+                    'foam_percent': foam_percent,
+                    'glint_percent': glint_percent,
+                    'water_percent': water_percent
+                }
+                data_index += 1
+
+            # Save dataset as JSON to file in the 'data' directory
+            solar_zenith_data[incoming_zenith] = dict(reflectances)
             pbar.update(1)
 
-        # Save dataset as JSON to file in the 'data' directory
-        solar_zenith_data[incoming_zenith] = dict(reflectances)
+        json_data = {
+            'wavelength': wavelength,
+            'wind_speed': wind_speed,
+            'data': solar_zenith_data
+        }
 
-    # Combine all data found in the 'data' directory into a single JSON file
-    combined_data = {}
-    for solar_zenith, data in solar_zenith_data.items():
-        combined_data[solar_zenith] = data
+        # Write the combined data to a file
+        with open(f'data/data_{wind_speed}ms_{wavelength}.json', 'w') as f:
+            json.dump(json_data, f)
+    except Exception as e:
+        print(f'Error: {e}')
 
-    # Write the combined data to a file
-    with open(f'data/data_{wind_speed}ms.json', 'w') as f:
-        json.dump(combined_data, f)
+        # Print the whole stack trace
+        import traceback
+        traceback.print_exc()
 
 if __name__ == "__main__":
     current_time = datetime.datetime.now()
-    pbar = tqdm(total=len(wind_speeds) * len(incoming_zeniths) * len(outgoing_zeniths))
+    pbar = tqdm(total=len(wind_speeds) * len(wavelengths) * len(incoming_zeniths))
     
-    pool = ThreadPool(8)
-    for wind_speed in wind_speeds:
-        pool.apply_async(generate_data, args=(wind_speed, pbar))
+    pool = ThreadPool(32)
+    for wavelength in wavelengths:
+        for wind_speed in wind_speeds:
+            pool.apply_async(generate_data, args=(wavelength, wind_speed, pbar))
     pool.close()
     pool.join()
     pbar.close()
--- notebooks/diff_test.py
+++ notebooks/diff_test.py
@@ -5,6 +5,7 @@ import drjit as dr
 import mitsuba as mi
 import numpy as np
 import matplotlib.pyplot as plt
+import imageio
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 mi.set_variant("llvm_ad_rgb_double")
@@ -28,6 +29,7 @@ wind_azimuth = 0
 comparison_data = {}
 
 incoming_zeniths = np.linspace(0, np.deg2rad(89), 18)
+incoming_limit = np.deg2rad(30)
 
 for wind_speed in wind_speeds:
     data_file = f'data/data_{wind_speed}ms.json'
@@ -49,9 +51,13 @@ for wind_speed in wind_speeds:
 
     differences = []
 
+    keys_as_floats = list(map(float, data.keys()))
+
     # Comparison loop for each solar zenith 
     for solar_zenith_key in data.keys():
         solar_zenith = float(solar_zenith_key)
+        if solar_zenith > incoming_limit:
+            continue
 
         # Get the reflectance data for the current solar zenith
         current_data = data[solar_zenith_key]
@@ -91,19 +97,47 @@ for wind_speed in wind_speeds:
 
         # Compute the difference between the data and the Mitsuba BSDF
         reflectances = np.array(reflectances)
-        reflectance_diff = relative_diff(reflectances, bsdf_array)
+        reflectance_diff = abs_diff(reflectances, bsdf_array)
         total_diff = np.sum(reflectance_diff)
         avg_diff = np.mean(reflectance_diff)
         differences.append(avg_diff) 
 
-        print(f'For solar zenith {solar_zenith}:')
-        print(f"  Total difference for solar zenith {solar_zenith}: {total_diff}")
-        print(f"  Average difference for solar zenith {solar_zenith}: {avg_diff}")
-        print(f'\n')
+        # Create a comparison plot between the data and the Mitsuba BSDF
+        
+
+        # Plot the data
+        fig, ax = plt.subplots()
+        ax.plot(zeniths, reflectances, label='Data')
+        ax.plot(zeniths, bsdf_array, label='Mitsuba BSDF')
+        ax.set_xlabel('Outgoing Zenith')
+        ax.set_ylabel('BRDF Value')
+        ax.set_title(f'Solar Zenith: {round(np.rad2deg(solar_zenith), 2)}')
+        
+        # Save the plot
+        plt.legend()
+
+        # Save the plot
+        plt.savefig(f'plots/comp_{wind_speed}_{solar_zenith}.png')
+        plt.close()
+
+        #print(f'For solar zenith {solar_zenith}:')
+        #print(f"  Total difference for solar zenith {solar_zenith}: {total_diff}")
+        #print(f"  Average difference for solar zenith {solar_zenith}: {avg_diff}")
+        #print(f'\n')
     comparison_data[wind_speed] = differences
+
+    limited_solar_zeniths = [zenith for zenith in incoming_zeniths if zenith <= incoming_limit]
+
+    filesnames = [f'plots/comp_{wind_speed}_{zenith}.png' for zenith in limited_solar_zeniths]
+
+    images = [imageio.imread(filename) for filename in filesnames]
+    imageio.mimsave(f'plots/comp_{wind_speed}.gif', images, duration=5)
 
 # Incoming zeniths to degrees
 incoming_zeniths = np.rad2deg(incoming_zeniths)
+
+# Filter those that are above the incoming limit
+incoming_zeniths = incoming_zeniths[incoming_zeniths <= np.rad2deg(incoming_limit)]
 
 # Plot the differences for each wind speed relative to the incoming zeniths
 fig, ax = plt.subplots()
